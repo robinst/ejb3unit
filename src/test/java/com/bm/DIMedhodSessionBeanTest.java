46fb2b181146313d2d83d8a7df87db9fba3f0a66
daniel_wiese <daniel_wiese@240b23ad-3c12-0410-ab44-d75537e93366>
2007-07-26 20:59:01 +0000

    setter injection
    git-svn-id: https://ejb3unit.svn.sourceforge.net/svnroot/ejb3unit/trunk/ejb3unit@152 240b23ad-3c12-0410-ab44-d75537e93366

---
+++ src/test/java/com/bm/DIMedhodSessionBeanTest.java
@@ -0,0 +1,57 @@
+package com.bm;
+
+import com.bm.data.bo.AnnotatedMethodsSessionBean;
+import com.bm.data.bo.StockWKNBo;
+import com.bm.testsuite.BaseSessionBeanFixture;
+
+/**
+ * This test test the injection using setter methods, as defined in chapter
+ * *.1.2 of the ejb3 specification 
+ * <code>
+ * @Resource(name=”customerDB”)
+ * public void setDataSource(DataSource myDB) {
+ * </code>
+ * 
+ * @author Daniel Wiese
+ */
+public class DIMedhodSessionBeanTest extends
+		BaseSessionBeanFixture<AnnotatedMethodsSessionBean> {
+
+	private static final Class[] usedBeans = { StockWKNBo.class };
+
+	/**
+	 * Constructor.
+	 */
+	public DIMedhodSessionBeanTest() {
+		super(AnnotatedMethodsSessionBean.class, usedBeans);
+	}
+
+	/**
+	 * Test the dpendency injection.
+	 * 
+	 * @author Daniel Wiese
+	 * @since 08.11.2005
+	 */
+	public void testDependencyInjection() {
+		final AnnotatedMethodsSessionBean toTest = this.getBeanToTest();
+		assertNotNull(toTest);
+		assertNotNull(toTest.getDs());
+		assertNotNull(toTest.getManager());
+		assertNotNull(toTest.getMySessionBean());
+		assertNotNull(toTest.getTimer());
+	}
+	
+	/**
+	 * Test the dpendency injection.
+	 * 
+	 * @author Daniel Wiese
+	 * @since 08.11.2005
+	 */
+	public void testDependencyInjectionWithMethodInvokation() {
+		final AnnotatedMethodsSessionBean toTest = this.getBeanToTest();
+		toTest.executeOperation();
+	}
+
+	
+
+}
---
+++ src/test/java/com/bm/PojoFixtureUsageExampleTest.java
@@ -0,0 +1,51 @@
+package com.bm;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.bm.data.bo.LineItem;
+import com.bm.data.bo.Order;
+import com.bm.testsuite.PoJoFixture;
+
+/**
+ * Shows the usage of the pojo fixture.
+ * 
+ */
+public class PojoFixtureUsageExampleTest extends PoJoFixture {
+
+	private static final Class<?>[] USED_ENTITIES = { Order.class,
+			LineItem.class };
+
+	public PojoFixtureUsageExampleTest() {
+		super(USED_ENTITIES);
+	}
+
+	public void testToWriteComplexObjectGraph() {
+		List<Order> complexObjectGraph = generateTestOrders();
+
+		// persist the graph and load it again
+		List<Order> persisted = persist(complexObjectGraph);
+		List<Order> allFromDB = findAll(Order.class);
+
+		// assert the persisted graph and the loaded are equal
+		assertCollectionsEqual(persisted, allFromDB);
+	}
+
+	public void testGetEntityManager() {
+		assertNotNull(this.getEntityManager());
+	}
+
+	private List<Order> generateTestOrders() {
+		final List<Order> orders = new ArrayList<Order>();
+		Order order = new Order();
+		order.setExpiration(new Date());
+		order.addPurchase("Testprod1", 30, 30.34);
+		order.addPurchase("Testprod2", 31, 31.34);
+		order.addPurchase("Testprod3", 32, 32.34);
+		order.addPurchase("Testprod4", 33, 33.34);
+		orders.add(order);
+		return orders;
+	}
+
+}
