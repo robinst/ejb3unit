1ca39b95215ed6371c862e72191784eb9126b5c8
bendt <bendt@240b23ad-3c12-0410-ab44-d75537e93366>
2009-11-13 15:56:22 +0000

    setValueForField now also tries to locate the field in all superclasses
    git-svn-id: https://ejb3unit.svn.sourceforge.net/svnroot/ejb3unit/trunk/ejb3unit@307 240b23ad-3c12-0410-ab44-d75537e93366

--- src/main/java/com/bm/testsuite/BaseTest.java
+++ src/main/java/com/bm/testsuite/BaseTest.java
@@ -17,142 +17,143 @@ import com.bm.utils.BeanEqualsTester;
  */
 public class BaseTest extends TestCase {
 
-	/**
-	 * Create an instance.
-	 * 
-	 * @param name
-	 *            The name of the test
-	 */
-	public BaseTest(final String name) {
-		super(name);
-	}
+    /**
+     * Create an instance.
+     *
+     * @param name
+     *            The name of the test
+     */
+    public BaseTest(final String name) {
+        super(name);
+    }
 
-	
-	/**
-	 * Default costructor.
-	 */
-	public BaseTest() {
-		super();
-	}
+    /**
+     * Default costructor.
+     */
+    public BaseTest() {
+        super();
+    }
 
+    /**
+     * Assert that the two collections are the same irrespective of order.
+     *
+     * @param a
+     *            The first collection
+     * @param b
+     *            The second collection
+     * @param <T> -
+     *            the type of the collection
+     */
+    public static <T> void assertCollectionsEqual(final Collection<? extends T> a,
+            final Collection<? extends T> b) {
+        BeanEqualsTester.testColletionsForEqual(a, b);
+    }
 
-	
-	/**
-	 * Assert that the two collections are the same irrespective of order.
-	 * 
-	 * @param a
-	 *            The first collection
-	 * @param b
-	 *            The second collection
-	 * @param <T> -
-	 *            the type of the collection
-	 */
-	public static <T> void assertCollectionsEqual(final Collection<? extends T> a,
-			final Collection<? extends T> b) {
-		BeanEqualsTester.testColletionsForEqual(a, b);
-	}
+    /**
+     * Assert that the specified condition is false. Older versions of junit
+     * have assertTrue() but not assertFalse so we add it here to be sure that
+     * it is present.
+     *
+     * @param description
+     *            The failure message to be used if the condition is not false.
+     * @param condition
+     *            The value to check.
+     */
+    public static void assertFalse(final String description,
+            final boolean condition) {
+        if (condition) {
+            fail(description + ": Expected false");
+        }
+    }
 
-	/**
-	 * Assert that the specified condition is false. Older versions of junit
-	 * have assertTrue() but not assertFalse so we add it here to be sure that
-	 * it is present.
-	 * 
-	 * @param description
-	 *            The failure message to be used if the condition is not false.
-	 * @param condition
-	 *            The value to check.
-	 */
-	public static void assertFalse(final String description,
-			final boolean condition) {
-		if (condition) {
-			fail(description + ": Expected false");
-		}
-	}
+    /**
+     * Assert that the specified condition is false. Older versions of junit
+     * have assertTrue() but not assertFalse so we add it here to be sure that
+     * it is present.
+     *
+     * @param condition
+     *            The value to check.
+     */
+    public static void assertFalse(final boolean condition) {
+        if (condition) {
+            fail("Expected false");
+        }
+    }
 
-	/**
-	 * Assert that the specified condition is false. Older versions of junit
-	 * have assertTrue() but not assertFalse so we add it here to be sure that
-	 * it is present.
-	 * 
-	 * @param condition
-	 *            The value to check.
-	 */
-	public static void assertFalse(final boolean condition) {
-		if (condition) {
-			fail("Expected false");
-		}
-	}
+    /**
+     * Assert that the specified object is an instance of this class.
+     *
+     * @param label
+     *            A description of the test
+     * @param object
+     *            The object to test
+     * @param clazz
+     *            The class
+     * @param <T> -
+     *            the type of the object
+     */
+    public static <T> void assertInstanceOf(final String label, final T object,
+            final Class<? extends T> clazz) {
+        if (!clazz.isAssignableFrom(object.getClass())) {
+            fail(label + ": object [" + object + "] is not an instance of class [" + clazz.getName() + "]");
+        }
+    }
 
-	/**
-	 * Assert that the specified object is an instance of this class.
-	 * 
-	 * @param label
-	 *            A description of the test
-	 * @param object
-	 *            The object to test
-	 * @param clazz
-	 *            The class
-	 * @param <T> -
-	 *            the type of the object
-	 */
-	public static <T>  void assertInstanceOf(final String label, final T object,
-			final Class<? extends T> clazz) {
-		if (!clazz.isAssignableFrom(object.getClass())) {
-			fail(label + ": object [" + object
-					+ "] is not an instance of class [" + clazz.getName() + "]");
-		}
-	}
+    /**
+     * Assert that the specified object is an instance of this class.
+     *
+     * @param object
+     *            The object to test
+     * @param clazz
+     *            The class
+     * @param <T> -
+     *            the type of the object
+     */
+    public static <T> void assertInstanceOf(final T object,
+            final Class<? extends T> clazz) {
+        assertInstanceOf("", object, clazz);
+    }
 
-	/**
-	 * Assert that the specified object is an instance of this class.
-	 * 
-	 * @param object
-	 *            The object to test
-	 * @param clazz
-	 *            The class
-	 * @param <T> -
-	 *            the type of the object
-	 */
-	public static <T> void assertInstanceOf(final T object,
-			final Class<? extends T> clazz) {
-		assertInstanceOf("", object, clazz);
-	}
+    /**
+     * We don´t want that test fails because they have no test method.
+     *
+     * @author Daniel Wiese
+     * @since 23.04.2006
+     */
+    public void testNothing() {
+        // intentionally left blank
+    }
 
-	/**
-	 * We don´t want that test fails because they have no test method.
-	 * 
-	 * @author Daniel Wiese
-	 * @since 23.04.2006
-	 */
-	public void testNothing() {
-		// intentionally left blank
-	}
+    /**
+     * Sets a value for a field in the tested-bean instance. this method also tries to locate the
+     * field in all superclasses of the specified object.
+     *
+     * @author Daniel Wiese
+     * @since 02.05.2006
+     * @param fieldName -
+     *            the name of the field
+     * @param toSet -
+     *            the value to set
+     */
+    public void setValueForField(Object forObject, String fieldName,
+            Object toSet) {
+        try {
+            Class clazz = forObject.getClass();
 
-	/**
-	 * Sets a value for a field in the tested-bean instance.
-	 * 
-	 * @author Daniel Wiese
-	 * @since 02.05.2006
-	 * @param fieldName -
-	 *            the name of the field
-	 * @param toSet -
-	 *            the value to set
-	 */
-	public void setValueForField(Object forObject, String fieldName,
-			Object toSet) {
-		try {
-			final Field field = forObject.getClass()
-					.getDeclaredField(fieldName);
-			Property prop = new Property(field);
-			prop.setField(forObject, toSet);
-		} catch (SecurityException e) {
-			throw new RuntimeException(e);
-		} catch (NoSuchFieldException e) {
-			throw new RuntimeException(e);
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	
+            while (clazz != null) {
+                try {
+                    final Field field = clazz.getDeclaredField(fieldName);
+                    Property prop = new Property(field);
+                    prop.setField(forObject, toSet);
+                    break;
+                } catch (NoSuchFieldException e) {
+                    clazz = clazz.getSuperclass();
+                }
+            }
+        } catch (SecurityException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
